9c179ae89d9cfe44140d6f51a79580b2e3193776
PauloEduardoCruz <pauloeduardo.crp@gmail.com>
2020-09-02 18:06:07 -0300

    Sistema modal implantado

--- winscript.js
+++ winscript.js
@@ -5,22 +5,7 @@ var jogador=10, pc=150;
 
 
 
-var url = window.location.href;
-url = url.split('?key=');
-url = url[2];
-var pontoss = url;
 
-
-
-
-
-
-
-
-var url = window.location.href;
-url = url.split('?key=');
-url = url[1];
-console.log('Chegou: ',url)
 
 
 
@@ -42,9 +27,9 @@ var msg = window.document.getElementById('grid-msg')
 // Pedra    5
 
 
-if(url == 1) {
-
-    document.getElementById('b1').style.border = "10px solid #eca922";
+function tesoura() {
+    
+    //document.getElementById('b1').style.border = "10px solid #eca922";
 
     jogador = 1;
 
@@ -53,7 +38,7 @@ if(url == 1) {
     console.log('Jogador:', jogador)
     console.log('aleatorio:', pc)
 
-
+/*
     switch (pc) {
         case 1:
             document.getElementById('segunda').src = "images/icon-scissors.svg";
@@ -72,6 +57,8 @@ if(url == 1) {
             document.getElementById('b2').style.border = "10px solid #dd405d"; 
             break;
     }
+*/
+
 
 
     if (jogador == pc) {
@@ -81,25 +68,26 @@ if(url == 1) {
         switch (pc) {
             case 2:
                 console.log('Spock derrete tesoura')
-                msg.innerHTML = `Spock derrete tesoura`
+                //msg.innerHTML = `Spock derrete tesoura`
                 rst = 1;
                 break;
             case 3:
                 console.log('Tesoura corta papel')
-                msg.innerHTML = `Tesoura corta papel`
+                //msg.innerHTML = `Tesoura corta papel`
                 break;
             case 4:
                 console.log('Tesoura decapita lagarto')
-                msg.innerHTML = `Tesoura decapita lagarto`
+                //msg.innerHTML = `Tesoura decapita lagarto`
                 break;
             case 5:
                 console.log('Pedra amassa tesoura') 
-                msg.innerHTML = `Pedra amassa tesoura`
+                //msg.innerHTML = `Pedra amassa tesoura`
                 rst = 1; 
                 break;
         } 
     }
 
+/*  
     if(rst==1){
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `YOU LOSE`  
@@ -109,7 +97,7 @@ if(url == 1) {
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `DRAW` 
     }
-
+*/
 
 }
 
@@ -140,15 +128,18 @@ if(url == 1) {
 
 
 
-if(url == 2) {
-    document.getElementById('primeiro').src = "images/icon-spock.svg";
-    document.getElementById('b1').style.border = "10px solid #52bed1";
+function spock() {
+    
+
+    //document.getElementById('primeiro').src = "images/icon-spock.svg";
+    //document.getElementById('b1').style.border = "10px solid #52bed1";
     jogador = 2;
 
     pc=Math.floor(Math.random() * 5) + 1 
     console.log('Jogador:', jogador)
     console.log('aleatorio:', pc)
 
+/*
     switch (pc) {
         case 1:
             document.getElementById('segunda').src = "images/icon-scissors.svg";
@@ -167,7 +158,7 @@ if(url == 2) {
             document.getElementById('b2').style.border = "10px solid #dd405d"; 
             break;
     }
-
+*/
 
 
     if (jogador == pc) {
@@ -177,26 +168,26 @@ if(url == 2) {
         switch (pc) {
             case 1:
                 console.log('Spock derrete tesoura')
-                msg.innerHTML = `Spock derrete tesoura`
+                //msg.innerHTML = `Spock derrete tesoura`
                 break;
             case 3:
                 console.log('Papel refuta Spock')
-                msg.innerHTML = `Papel refuta Spock`
+                //msg.innerHTML = `Papel refuta Spock`
                 rst = 1;
                 break;
             case 4:
                 console.log('Lagarto envenena Spock')
-                msg.innerHTML = `Lagarto envenena Spock`
+                //msg.innerHTML = `Lagarto envenena Spock`
                 rst = 1;
                 break;
             case 5:
                 console.log('Spock vaporiza pedra') 
-                msg.innerHTML = `Spock vaporiza pedra`
+                //msg.innerHTML = `Spock vaporiza pedra`
                 break;
         } 
     }
 
-    
+/*  
     if(rst==1){
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `YOU LOSE`  
@@ -206,7 +197,7 @@ if(url == 2) {
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `DRAW` 
     }
-
+*/
 
 }
 
@@ -237,10 +228,10 @@ if(url == 2) {
 
 
 
-if(url == 3) {
-
-    document.getElementById('primeiro').src = "images/icon-paper.svg";
-    document.getElementById('b1').style.border = "10px solid #5671f5";
+function papel() {
+    
+   // document.getElementById('primeiro').src = "images/icon-paper.svg";
+   // document.getElementById('b1').style.border = "10px solid #5671f5";
 
 
     jogador = 3;
@@ -249,7 +240,7 @@ if(url == 3) {
     console.log('Jogador:', jogador)
     console.log('aleatorio:', pc)
 
-
+/*
     switch (pc) {
         case 1:
             document.getElementById('segunda').src = "images/icon-scissors.svg";
@@ -268,7 +259,7 @@ if(url == 3) {
             document.getElementById('b2').style.border = "10px solid #dd405d"; 
             break;
     }
-
+*/
 
 
     if (jogador == pc) {
@@ -278,26 +269,26 @@ if(url == 3) {
         switch (pc) {
             case 1:
                 console.log('Tesoura corta papel')
-                msg.innerHTML = `Tesoura corta papel` 
+                //msg.innerHTML = `Tesoura corta papel` 
                 rst = 1;
                 break;
             case 2:
                 console.log('Papel refuta Spock')
-                msg.innerHTML = `Papel refuta Spock` 
+                //msg.innerHTML = `Papel refuta Spock` 
                 break;
             case 4:
                 console.log('Lagarto come papel')
-                msg.innerHTML = `Lagarto come papel` 
+                //msg.innerHTML = `Lagarto come papel` 
                 rst = 1;
                 break;
             case 5:
                 console.log('Papel cobre pedra') 
-                msg.innerHTML = `Papel cobre pedra` 
+                //msg.innerHTML = `Papel cobre pedra` 
                 break;
         } 
     }
 
-
+/*
     if(rst==1){
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `YOU LOSE`  
@@ -307,7 +298,7 @@ if(url == 3) {
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `DRAW` 
     }
-
+*/
 
 
 }
@@ -345,10 +336,13 @@ if(url == 3) {
 
 
 
-if(url == 4) {
 
-    document.getElementById('primeiro').src = "images/icon-lizard.svg";
-    document.getElementById('b1').style.border = "10px solid #8c5de5";
+function lagarto() {
+    
+
+
+  //  document.getElementById('primeiro').src = "images/icon-lizard.svg";
+ //   document.getElementById('b1').style.border = "10px solid #8c5de5";
 
     jogador = 4;
 
@@ -356,7 +350,7 @@ if(url == 4) {
     console.log('Jogador:', jogador)
     console.log('aleatorio:', pc)
 
-
+/*
     switch (pc) {
         case 1:
             document.getElementById('segunda').src = "images/icon-scissors.svg";
@@ -375,7 +369,7 @@ if(url == 4) {
             document.getElementById('b2').style.border = "10px solid #dd405d"; 
             break;
     }
-
+*/
 
 
     if (jogador == pc) {
@@ -385,26 +379,26 @@ if(url == 4) {
         switch (pc) {
             case 1:
                 console.log('Tesoura decapita lagarto')
-                msg.innerHTML = `Tesoura decapita lagarto`
+                //msg.innerHTML = `Tesoura decapita lagarto`
                 rst = 1;
                 break;
             case 2:
                 console.log('Lagarto envenena Spock')
-                msg.innerHTML = `Lagarto envenena Spock`
+                //msg.innerHTML = `Lagarto envenena Spock`
                 break;
             case 3:
                 console.log('Lagarto come papel')
-                msg.innerHTML = `Lagarto come papel` 
+                //msg.innerHTML = `Lagarto come papel` 
                 break;
             case 5:
                 console.log('Pedra esmaga lagarto')
-                msg.innerHTML = `Pedra esmaga lagarto` 
+                //msg.innerHTML = `Pedra esmaga lagarto` 
                 rst = 1;
                 break;
         } 
     }
 
-
+    /*
     if(rst==1){
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `YOU LOSE`  
@@ -414,7 +408,7 @@ if(url == 4) {
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `DRAW` 
     }
-
+    */
 
 }
 
@@ -437,18 +431,14 @@ if(url == 4) {
 
 
 
+ var url=0;
 
 
 
-
-
-
-
-if(url == 5) {
-
-
-    document.getElementById('primeiro').src = "images/icon-rock.svg"; 
-    document.getElementById('b1').style.border = "10px solid #dd405d";
+function pedra() {
+    
+    //document.getElementById('primeiro').src = "images/icon-rock.svg"; 
+    //document.getElementById('b1').style.border = "10px solid #dd405d";
 
     jogador = 5;
 
@@ -457,7 +447,7 @@ if(url == 5) {
     console.log('aleatorio:', pc)
 
 
-
+/*
     //estética
     switch (pc) {
         case 1:
@@ -477,7 +467,7 @@ if(url == 5) {
             document.getElementById('b2').style.border = "10px solid #dd405d"; 
             break;
     }
-
+*/
 
     //lógica 
     if (jogador == pc) {
@@ -487,25 +477,26 @@ if(url == 5) {
         switch (pc) {
             case 1:
                 console.log('Pedra amassa tesoura')
-                msg.innerHTML = `Pedra amassa tesoura` 
+                //msg.innerHTML = `Pedra amassa tesoura` 
                 break;
             case 2:
                 console.log('Spock vaporiza pedra')
-                msg.innerHTML = `Spock vaporiza pedra` 
+                //msg.innerHTML = `Spock vaporiza pedra` 
                 rst = 1;
                 break;
             case 3:
                 console.log('Papel cobre pedra')
-                msg.innerHTML = `Papel cobre pedra` 
+                //msg.innerHTML = `Papel cobre pedra` 
                 rst = 1;
                 break;
             case 4:
                 console.log('Pedra esmaga lagarto')
-                msg.innerHTML = `Pedra esmaga lagarto` 
+                //msg.innerHTML = `Pedra esmaga lagarto` 
                 break;
         } 
     }
 
+    /*
     if(rst==1){
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `YOU LOSE`  
@@ -515,7 +506,7 @@ if(url == 5) {
         var res = window.document.getElementById('texto-win')
         res.innerHTML = `DRAW` 
     }
-
+    */
 
 }
 
